ce8191fd831ca68e5315aab9cef6d0c1b1fa0007
= <jo123.garcia.s@gmail.com>
2018-10-15 14:48:21 -0500

    events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = { mouseX: 0, mouseY: 0};
+  }
+  handleClick (e) {
+    console.log(e.nativeEvent);
+    console.log('Hi Handle!')
+  }
+  handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    this.setState({ mouseX: clientX, mouseY: clientY});
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h4>Events:</h4>
+        <button onClick={() => console.log('Hi!')}>Click here!</button>
+        <h4>Synthetic Events</h4>
+        <button onClick={this.handleClick}>Handle Click</button>
+        <h4>Mouse events</h4>
+        <div 
+          style={{
+            border: '1px solid grey', margin: 10, padding:10,
+          }}
+          onMouseMove={this.handleMouseMove}
+        >
+          <p>{this.state.mouseX}, {this.state.mouseY}</p>
+        </div>
       </div>
     );
   }
